14dc14262c6464cf5840156daeea16dde1b32c3b
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-11-04 22:53:52 +0000

    better insertion sort from StackOverflow

--- haskell/InsertionSort.hs
+++ haskell/InsertionSort.hs
@@ -4,16 +4,27 @@ import Data.List (foldl')
 insertionSort :: (Ord n) => [n] -> [n]
 insertionSort [] = []
 insertionSort (x:xs) =
-  foldl' insertion [x] xs
+  foldl' insert [x] xs
   where
-    insertion [] y = [y]
-    insertion ys y =
+    insert [] y = [y]
+    insert ys y =
       let lastEl = last ys
       in
         if lastEl > y
-          then insertion (init ys) y <> [lastEl]
+          then insert (init ys) y <> [lastEl]
           else ys <> [y]
+
+-- from: https://stackoverflow.com/questions/28550361/insertion-sort-in-haskell
+insertionSort' :: (Ord n) => [n] -> [n]
+insertionSort' [] = []
+insertionSort' [x] = [x]
+insertionSort' (x:xs) = insert $ insertionSort' xs
+  where insert [] = [x]
+        insert (y:ys)
+          | x <= y = x:y:ys
+          | x > y = y:insert ys
+
 
 main :: IO ()
 main = do
-  U.test insertionSort
+  U.test insertionSort'
